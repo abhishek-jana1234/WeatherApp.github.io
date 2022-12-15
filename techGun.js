ea4c6ece5fe88028b975627f56538531cf389240
abhishek-jana1234 <120671140+abhishek-jana1234@users.noreply.github.com>
2022-12-15 22:35:05 +0530

    Update techGun.js

--- techGun.js
+++ techGun.js
@@ -1,8 +1,7 @@
 const options = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "23daf53b9dmshc3a38038f690d5ap11ff13jsn93e59a55af2c",
-    "X-RapidAPI-Host": "weather-by-api-ninjas.p.rapidapi.com",
+    
   },
 };
 const getWeather = (city) => {
